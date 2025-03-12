f1f490576296ccfae9705365a1b70b15a3a5c207
Didar <aizharyk78@gmail.com>
2025-03-12 15:46:56 +0500

    country

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Привет, React!</h1>
+      <p>Начинаю вёрстку 🚀</p>
+    </div>
+  );
+}
+
+export default App;
